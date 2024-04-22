24d8d9e9acd8666cec1edf04edaff73020309fab
Yagrr <90156764+Yagrr@users.noreply.github.com>
2024-04-22 02:03:16 +0100

    Add dynamic header script with autohide on scroll Style new background and bigsign background

---
+++ header.js
@@ -0,0 +1,46 @@
+const header = document.querySelector('.top-header');
+const hero = document.querySelector('.hero');
+let lastScroll = 0;
+
+
+/* Avoids lag */
+/* https://daily-dev-tips.com/posts/show-and-hide-a-header-based-on-scroll-direction /*/
+const throttle = (func, time = 100) => {
+  let lastTime = 0;
+  return () => {
+    const now = new Date();
+    if (now - lastTime >= time) {
+      func();
+      time = now;
+    }
+  };
+};
+
+const validateHeader = () => {
+    const windowY = window.scrollY;
+    const windowH = window.innerHeight;
+
+    if (windowY > windowH) {
+        // We passed the first section, set a toggable class
+        header.classList.add('is-fixed');
+    } else {
+        header.classList.remove('is-fixed', 'can-animate');
+    }
+
+    if (windowY < lastScroll) {
+        header.classList.add('scroll-up');
+    } else {
+        header.classList.remove('scroll-up');
+    }
+
+    // Determine is we ready to animate
+    if (windowY > windowH + 40) {
+        header.classList.add('can-animate');
+    } else {
+        header.classList.remove('scroll-up');
+    }
+
+    lastScroll = windowY;
+}
+
+window.addEventListener('scroll',throttle(validateHeader,100));
